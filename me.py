d1f7718f3a0e771d310304303b229cd9a4799da6
EkaterinaShirmankina <48629669+EkaterinaShirmankina@users.noreply.github.com>
2019-05-20 12:18:29 +0300

    Union of equal debts (upgraded code)

--- me.py
+++ me.py
@@ -1,16 +1,27 @@
-output_list = []
+# [0] = дата, [1] = задолжность
+fee_feedback_final = [[9, 0, 0],
+                     [10, 0, 0],
+                     [11, 0, 0],
+                     [12, 408.0, 8.16],
+                     [13, 3476.0, 69.52],
+                     [14, 3476.0, 69.52],
+                     [14, 3476.0, 69.52],
+                     [15, 2911.0, 58.22]]
 
-fff_start = fee_fedback_final[0][0]
-for i in range(0, len(fee_fedback_final)):
-    if i + 1 <= len(fee_fedback_final) - 1:
-        if fee_fedback_final[i][1] != fee_fedback_final[i + 1][1]:
-            output_list.append((f'За {fee_fedback_final[i][0] - fff_start + 1} '
-                  f'дней с {fff_start} по {fee_fedback_final[i][0]}'
-                  f'задолженность составила {(fee_fedback_final[i][0] - fff_start + 1) * fee_fedback_final[i][1]}'
-                  f' штраф {(fee_fedback_final[i][0] - fff_start + 1) * fee_fedback_final[i][2]}'))
-            fff_start = fee_fedback_final[i + 1][0]
-    else:
-        output_list.append((f'За {fee_fedback_final[i][0] - fff_start + 1} '
-                  f'дней с {fff_start} по {fee_fedback_final[i][0]}'
-                  f'задолженность составила {(fee_fedback_final[i][0] - fff_start + 1) * fee_fedback_final[i][1]}'
-                  f' штраф {(fee_fedback_final[i][0] - fff_start + 1) * fee_fedback_final[i][2]}'))
+#нужно посчитать количество дней, когда задолжность была одинакова.
+def makeReport(f):
+    output_list = []
+    begin = f[0][0]
+    for i in range(len(f)):
+        circle_body = i + 1 <= len(f) - 1
+        if circle_body and f[i][1] != f[i + 1][1] or not circle_body:
+            output_list.append((f'За {f[i][0] - begin + 1} ' +
+                (f'дней с {begin} по {f[i][0]} ' if f[i][0] - begin > 0 else f'день {begin} числа ') +
+                f'задолженность составила {(f[i][0] - begin + 1) * f[i][1]}' +
+                f' штраф {(f[i][0] - begin + 1) * f[i][2]}'))
+        if circle_body and f[i][1] != f[i + 1][1]:
+            begin = f[i + 1][0]
+    return output_list
+            
+print(*makeReport(fee_feedback_final), sep='\n')
+#print(makeReport(fee_feedback_final))
